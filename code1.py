93e68711b9dcab66ec34e8a0cfcf4df722b40cfe
hossamfathalla <93450995+hossamfatahalla@users.noreply.github.com>
2022-10-30 13:51:46 +0200

    Create code1.py

---
+++ code1.py
@@ -0,0 +1,15 @@
+no1=int(input("enter first no :"))
+no2=int(input("enter sec no : "))
+oper=input("enter oper:")
+
+
+if oper == "+":
+    result= no1+no2
+
+elif oper == "-":
+    result= no1-no2
+
+elif oper == "*":
+    result= no1*no2
+
+print(result)    
